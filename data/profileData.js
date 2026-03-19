2620a4f0d80c2b19df597fa7c35b09110437eec6
BecomeHuman42 <zhangrenren2024@yeah.net>
2026-03-19 11:51:17 -0400

    feat: add simulated user data for testing

--- data/profileData.js
+++ data/profileData.js
@@ -1,1 +1,21 @@
-export let isLoggedIn = true;
+// data/profileData.js
+
+export const users = [
+    {
+        id: "12345678", 
+        username: "John",
+        password: "123", 
+        name: "John Smith",
+        email: "john@example.com",
+        phone: "514-000-0000",
+        role: "Student",
+        emergencyContact: "514-999-9999",
+        accessibility: "none" 
+    }
+];
+
+// Login status
+export let isLoggedIn = false;
+
+// Current User
+export let currentUser = null;
