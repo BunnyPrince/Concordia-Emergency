fff51315226d0d9511f18177b78bd3a4a44ff830
Yuhao <yuhosan@163.com>
2026-03-21 13:54:06 -0400

    Merge the functions

--- scripts/hazardsPages/elevator.js
+++ scripts/hazardsPages/elevator.js
@@ -1,8 +1,8 @@
-import { building } from "../../data/building.js";
+import { buildings } from "../../data/building.js";
 
 const select = document.getElementById("building");
 
-building.forEach((b) => {
+buildings.forEach((b) => {
   const option = document.createElement("option");
   option.value = b.buildingName;
   option.textContent = b.buildingName;
