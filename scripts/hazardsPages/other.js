d39aec6e398646fc37f8552e5b938411bbe8c5da
Yuhao <yuhosan@163.com>
2026-03-21 13:28:46 -0400

    Restore report pages

--- scripts/hazardsPages/other.js
+++ scripts/hazardsPages/other.js
@@ -11,8 +11,26 @@ building.forEach((b) => {
 
 
 document.addEventListener("DOMContentLoaded", () => {
+  localStorage.removeItem("report"); 
   document.getElementById('description').addEventListener('input', function() {
     this.style.height = 'auto';                 
     this.style.height = this.scrollHeight + 'px'; 
   });
+});
+
+document.getElementById("forward").addEventListener("click", function () {
+    const photoInput = document.getElementById("photo");
+    const photoFiles = Array.from(photoInput.files).map(file => file.name);
+
+    const data = {
+        "Hazard Type": "Other",
+        "Building": document.getElementById("building").value,
+        "Intersection Street 1": document.getElementById("street1").value,
+        "Intersection Street 2": document.getElementById("street2").value,
+        "Impact Type": document.getElementById("impact").value,
+        "Description": document.getElementById("description").value,
+        "Photo": photoFiles
+    };
+
+    localStorage.setItem("report", JSON.stringify(data));
 });
